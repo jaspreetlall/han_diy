f2bc1d79114a8705a878f2a6ef7eb0bf1f5cb2a3
Jaspreet Singh Lall <jaspreet.lall@gmail.com>
2021-01-26 15:15:43 -0800

    Styled home component

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
+import React from 'react';
+import './Home.scss';
 
 function Home() {
   return (
-    <div>
-      Home Page
-    </div>
+    <section className="home">
+      <div className="home__block">
+        <div className="home__block-hero">
+          <h1 className="home__block-hero-title container">Do. It.<br/> Yourself.</h1>
+        </div>
+        <div className="home__block-body container">
+          <h2 className="home__block-body-title">Get started</h2>
+          <div className="home__block-body-para">
+            <h3 className="home__block-body-para-title">Create</h3>
+            <p className="home__block-body-para-text">Save your do-it-yourself ideas to Han-DIY to keep them handy.</p>
+          </div>
+          <div className="home__block-body-para">
+            <h3 className="home__block-body-para-title">View</h3>
+            <p className="home__block-body-para-text">Peep into your saved ideas for a list of parts, tools or additional notes to get you going.</p>
+          </div>
+          <div className="home__block-body-para">
+            <h3 className="home__block-body-para-title">Complete</h3>
+            <p className="home__block-body-para-text">Mark your ideas as complete when you turn those into real world projects.</p>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
