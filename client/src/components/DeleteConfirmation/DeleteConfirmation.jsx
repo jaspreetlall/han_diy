eec7e0bf273b0455c8d9e22ef8de81cdc6bef0de
Jaspreet Singh Lall <jaspreet.lall@gmail.com>
2021-01-28 18:52:07 -0800

    Styled delete modal component

--- client/src/components/DeleteConfirmation/DeleteConfirmation.jsx
+++ client/src/components/DeleteConfirmation/DeleteConfirmation.jsx
@@ -1,1 +1,28 @@
- 
+import React from 'react';
+import './DeleteConfirmation.scss';
+
+function DeleteConfirmation({ideaId, cancelButtonHandler}) {
+
+  // TODO - Implement Delete functionality
+  console.log(ideaId);
+
+  return (
+    <div className="modal">
+      <div className="modal__block container">
+        <div className="modal__block-message">
+          <h3 className="modal__block-message-title">Delete idea?</h3>
+          <p className="modal__block-message-para">This action cannot be undone.</p>
+        </div>
+        <div className="modal__block-buttons">
+          <button
+            className="modal__block-buttons-cancel"
+            onClick={cancelButtonHandler}
+          >Keep</button>
+          <button className="modal__block-buttons-delete">Delete</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default DeleteConfirmation
