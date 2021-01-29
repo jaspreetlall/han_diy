70a5e00217519023b1a8a1d42543c213f1398cbc
Jaspreet Singh Lall <jaspreet.lall@gmail.com>
2021-01-29 15:20:30 -0800

    Added create idea functionality

--- client/src/components/IdeaCreate/IdeaCreate.jsx
+++ client/src/components/IdeaCreate/IdeaCreate.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './IdeaCreate.scss';
 import CancelIcon from '../../assets/icons/cancel-white-48dp.svg';
 import AddIcon from '../../assets/icons/create-white-48dp.svg';
+import Axios from 'axios';
 
-function IdeaCreate() {
+const ideaUrl = "http://localhost:8080/idea/";
+
+// TODO - Redirect to idea after creating.
+
+function IdeaCreate(props) {
 
   const categories =
   [
@@ -16,14 +21,40 @@ function IdeaCreate() {
     "Other"
   ];
 
-  // TODO - implement create idea functionality
-  // TODO - add form validation
+  const [formData, setFormData] = useState({
+    userId: "2fc8e7ee-ee37-483f-93dc-116389646d4f",
+    title: '',
+    imageUrl: '',
+    description: '',
+    category: '',
+    tools: '',
+    parts: '',
+    link: '',
+    notes: ''
+  })
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    Axios
+    .post(ideaUrl, formData)
+    .then(props.history.push('/'))
+    .catch(err => console.log(err))
+  }
+
+  const handleChange = (e) => {
+    const {name, value} = e.target;
+    setFormData(prev => ({...prev, [name]: value}))
+  }
 
   return (
     <section className="create">
       <div className="create__block container">
         <h2 className="create__block-title">Create new idea</h2>
-        <form className="create__block-form" id="createForm">
+        <form
+          className="create__block-form"
+          onSubmit={submitHandler}
+          id="createForm"
+        >
           <div className="create__block-form-input">
             <label className="create__block-form-input-label" htmlFor="title">Title</label>
             <input
@@ -31,13 +62,20 @@ function IdeaCreate() {
               type="text"
               id="title"
               name="title"
-              placeholder="Title for your idea"
-            />
+              required
+              autoFocus
+              value={formData.title}
+              onChange={handleChange}
+              placeholder="Title for your idea"/>
           </div>
           <div className="create__block-form-input">
             <label className="create__block-form-input-label" htmlFor="category">Category</label>
-            <select className="create__block-form-input-field" name="category" id="category">
-              <option selected="true" disabled="disabled">Select Category</option>
+            <select
+              className="create__block-form-input-field"
+              name="category"
+              value={formData.category}
+              onChange={handleChange}
+              id="category">
               {categories.map((category, index) => {
                 return(
                   <option value={category} key={index}>{category}</option>
@@ -52,6 +90,9 @@ function IdeaCreate() {
               type="text"
               id="description"
               name="description"
+              required
+              value={formData.description}
+              onChange={handleChange}
               placeholder="Description of your idea"
             />
           </div>
@@ -62,6 +103,8 @@ function IdeaCreate() {
               type="text"
               id="parts"
               name="parts"
+              value={formData.parts}
+              onChange={handleChange}
               placeholder="Parts needed. Separate with commas."
             />
           </div>
@@ -72,6 +115,8 @@ function IdeaCreate() {
               type="text"
               id="tools"
               name="tools"
+              value={formData.tools}
+              onChange={handleChange}
               placeholder="Tools needed. Separate with commas."
             />
           </div>
@@ -82,6 +127,8 @@ function IdeaCreate() {
               type="text"
               id="link"
               name="link"
+              value={formData.link}
+              onChange={handleChange}
               placeholder="External reference link"
             />
           </div>
@@ -92,6 +139,8 @@ function IdeaCreate() {
               type="text"
               id="notes"
               name="notes"
+              value={formData.notes}
+              onChange={handleChange}
               placeholder="Additional reference notes"
             />
           </div>
