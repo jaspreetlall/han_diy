4771b8b8b01da988e7494c261a042dc712789d10
Jaspreet Singh Lall <jaspreet.lall@gmail.com>
2021-02-01 18:50:49 -0800

    Improved code logic and readability in ideaRoute.js. Minor tweaks in data.json

--- server/routes/ideaRoute.js
+++ server/routes/ideaRoute.js
@@ -14,25 +14,34 @@ const splitStringAtCommaToArray = (stringToSplit) => {
 router
 // Endpoint to get the list of all ideas
 .get('/', (_request, response) => {
-  let ideaList = [];
-  ideas.forEach((idea) => {
-    let ideaObject = {
-      "id": idea.id,
-      "title": idea.title,
-      "imageUrl": idea.imageUrl,
-      "category" : idea.category,
-      "done": idea.done,
-      "timestamp": idea.timestamp
+  // Reading data file for up-to-date data
+  fs.readFile('./data/data.json', (err, data) => {
+    if(err) throw err;
+    else {
+      // Storing existing ideas into an array
+      let originalIdeasArray = JSON.parse(data);
+      let ideaList = [];
+      // Creating idea object with selective data
+      originalIdeasArray.forEach((idea) => {
+        let ideaObject = {
+          "id": idea.id,
+          "title": idea.title,
+          "imageUrl": idea.imageUrl,
+          "category" : idea.category,
+          "done": idea.done,
+          "timestamp": idea.timestamp
+        }
+        // Pushing into the array to be sent as a response
+        ideaList.push(ideaObject);
+      })
+      // Returning the idea array sorted by newest ideas at 1st
+      // based on the timestamp
+      return response.status(200).send(ideaList.sort((a,b) => b.timestamp - a.timestamp));
     }
-    ideaList.push(ideaObject);
   })
-  // Returning the idea array sorted by newest ideas at 1st
-  // based on the timestamp
-  return response.status(200).send(ideaList.sort((a,b) => b.timestamp - a.timestamp));
 })
 // Endpoint to post an idea
 .post('/', (request, response) => {
-
   let ideaToBeAdded = {
     "id": uuid(),
     "userId": request.body.userId,
@@ -49,7 +58,7 @@ router
     "notes": request.body.notes,
     "timestamp": Date.now()
   }
-
+  // Reading data file for up-to-date data
   fs.readFile('./data/data.json', (err, data) => {
     if(err) throw err;
     // Storing existing ideas into an array
@@ -59,18 +68,42 @@ router
     let ideasArrayWithNewIdea = [...originalIdeasArray, ideaToBeAdded];
     // Writing the new array to the file
     fs.writeFile('./data/data.json',
-      JSON.stringify(ideasArrayWithNewIdea), err => console.log(err))
+    JSON.stringify(ideasArrayWithNewIdea, null, 2), err => {
+      if(err) console.log(err);
+      else {
+        // Reading data file for up-to-date data
+        fs.readFile('./data/data.json', (err, data) => {
+          if(err) console.log(err);
+          else {
+            // Storing existing ideas into an array
+            let originalIdeasArray = JSON.parse(data);
+            // Finding and storing new idea
+            let ideaJustAdded = originalIdeasArray.find(idea => idea.id === ideaToBeAdded.id);
+            // Returning the newly saved idea object as response 
+            return response.status(201).send(ideaJustAdded);
+          }
+        })
+      }
+    })
   })
-
-  return response.status(201).send(ideaToBeAdded);
 })
 
 router
 // Endpoint to get idea by id
 .get('/:id', (request, result) => {
   let idOfRequestedIdea = request.params.id;
-  let requestedIdea = ideas.find(idea => idea.id === idOfRequestedIdea);
-  return result.status(200).send(requestedIdea);
+  // Reading data file for up-to-date data
+  fs.readFile('./data/data.json', (err, data) => {
+    if(err) throw err;
+    else {
+      // Storing existing ideas into an array
+      let originalIdeasArray = JSON.parse(data);
+      // Finding & storing the idea object for requested idea
+      let requestedIdea = originalIdeasArray.find(idea => idea.id === idOfRequestedIdea);
+      // Returning the requested idea
+      return result.status(200).send(requestedIdea);
+    }
+  })
 })
 
 // Endpoint to update existing idea by id
@@ -79,7 +112,7 @@ router
   let updatedIdea
   // Retrieve idea to update
   ideaToUpdate = ideas.find(idea => idea.id === idOfIdeaToUpdate);
-
+  // Validating data received before writing to file
   if(ideaToUpdate.id) {
     updatedIdea = {
       "id": ideaToUpdate.id,
@@ -97,29 +130,32 @@ router
       "notes": request.body.notes,
       "timestamp": ideaToUpdate.timestamp
     }
-  
+    // Reading data file for up-to-date data
     fs.readFile('./data/data.json', (err, data) => {
       if(err) throw err;
       // Storing existing ideas into an array
       let originalIdeasArray = JSON.parse(data);
-      
       // Storing all ideas except the one to be update
       let ideasArrayWithoutUpdatedIdea = originalIdeasArray.filter(idea => idea.id !== idOfIdeaToUpdate);
-  
       // New array with updated idea
       let ideasArrayIncludeUpdatedIdea = [...ideasArrayWithoutUpdatedIdea, updatedIdea];
-  
       // Writing the new array to the file
       fs.writeFile('./data/data.json',
-        JSON.stringify(ideasArrayIncludeUpdatedIdea), err => console.log(err))
+      JSON.stringify(ideasArrayIncludeUpdatedIdea, null, 2), err => {
+        if(err) throw err;
+        else {
+          // Sending the updated idea after successful write
+          return response.status(200).send(updatedIdea);
+        }
+      })
     })
   } else return res.status(400).send("Please send valid data in correct format.")
-  return response.status(200).send(updatedIdea);
 })
 
 // Endpoint to delete idea by id
 .delete('/:id', (request, response) => {
   let idOfIdeaToDelete = request.params.id;
+  // Reading data file for up-to-date data
   fs.readFile('./data/data.json', (err, data) => {
     if(err) throw err;
     // Storing existing ideas into an array
@@ -128,9 +164,14 @@ router
     let ideasArrayAfterDelete = originalIdeasArray.filter(idea => idea.id !== idOfIdeaToDelete);
     // Writing the new array to the file
     fs.writeFile('./data/data.json',
-      JSON.stringify(ideasArrayAfterDelete), err => console.log(err))
+    JSON.stringify(ideasArrayAfterDelete, null, 2), err => {
+      if(err) console.log(err);
+      else {
+        // Sending response after successful delete
+        return response.status(200).send("Deleted successfully.");
+      }
+    })
   })
-  return response.status(200).send(`${idOfIdeaToDelete} was successfully deleted.`);
 })
 
 module.exports = router;
