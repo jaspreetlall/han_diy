545b50534da6de94047492e10db7cf2e932a5880
Jaspreet Singh Lall <jaspreet.lall@gmail.com>
2021-02-14 18:09:20 -0800

    Added login and signup buttons

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,11 +1,18 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useContext } from 'react';
+import { Link } from 'react-router-dom';
 import './Home.scss';
+import { FireAuthContext } from "../../Firebase/AuthProvider";
+
 
 function Home() {
-
+  // Setting page title
   useEffect(() => {
     document.title = "Han-DIY | Home";
   }, []);
+  
+  // Getting user from context to check logged in status
+  // for not showing login/signup buttons if logged in.
+  const {currentUser} = useContext(FireAuthContext);
 
   return (
     <section className="home">
@@ -15,6 +22,16 @@ function Home() {
         </div>
         <div className="home__block-body container">
           <h2 className="home__block-body-title">Get started</h2>
+          {/* Displaying the links to login and signup only if
+              the user is not logged in
+          */}
+          { !currentUser
+              ? <div className="home__block-body-para home__block-body-para--links">
+                  <Link className="home__block-body-para-link" to="/signup">Sign up</Link>
+                  <Link className="home__block-body-para-link home__block-body-para-link--login" to="/login">Log in</Link>
+                </div>
+              : <div></div>
+          }
           <div className="home__block-body-para">
             <h3 className="home__block-body-para-title">Create</h3>
             <p className="home__block-body-para-text">Save your do-it-yourself ideas to Han-DIY to keep them handy.</p>
